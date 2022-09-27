925339b09f727a837cfc6075fbb269ab717f9ca3
Abhishek Verma <abhishek.verma@canonical.com>
2022-09-27 15:37:40 +0530

    Bundling kubectl binary with the snap. Requiring kubeconfig and clustername as parameters for all actions.

--- helpers/setup.py
+++ helpers/setup.py
@@ -2,7 +2,7 @@ import sys
 import os
 import yaml
 
-kubectl_cmd='kubectl'
+kubectl_cmd='{}/kubectl'.format(os.environ['SNAP'])
 
 def extract_ca_crt_from_kube_config(kubeconfig: str, cluster_name: str = None) -> None:
     with open(kubeconfig) as f:
@@ -39,38 +39,54 @@ def extract_ca_crt_from_kube_config(kubeconfig: str, cluster_name: str = None) -
         os.system('\n\necho {} | base64 --decode'.format(certs[int(cluster_id)]))
 
 
-def setup_user(username: str, namespace: str) -> None:
+def setup_user(username: str, namespace: str, kubeconfig: str, cluster_name: str) -> None:
     clusterrolebindingname = username + '-role'
     clusterroleaccess='view'
-    os.system("{} create serviceaccount {} --namespace={}".format(kubectl_cmd, username, namespace))
-    os.system("{} create clusterrolebinding {} --clusterrole={}  --serviceaccount={}:{} --namespace={}".format(kubectl_cmd, clusterrolebindingname, clusterroleaccess, namespace, username, namespace))
+    os.system("{} create serviceaccount --kubeconfig={} --cluster={} {} --namespace={}".format(kubectl_cmd, kubeconfig, cluster_name, username, namespace))
+    os.system("{} create clusterrolebinding --kubeconfig={} --cluster={} {} --clusterrole={}  --serviceaccount={}:{} --namespace={}".format(kubectl_cmd, kubeconfig, cluster_name, clusterrolebindingname, clusterroleaccess, namespace, username, namespace))
 	
 
-def dump_ca_crt(secretname: str) -> None:
-    os.system('{} get secret {} -o jsonpath="{{.data.ca\.crt}}" | base64 --decode'.format(kubectl_cmd, secretname))
+def dump_ca_crt(secretname: str, kubeconfig: str, cluster_name: str) -> None:
+    os.system('{} get secret --kubeconfig={} --cluster={} {} -o jsonpath="{{.data.ca\.crt}}" | base64 --decode'.format(kubectl_cmd,  kubeconfig, cluster_name, secretname))
 
 
-def dump_token(secretname: str) -> None:
-    os.system('{} get secret {} -o jsonpath="{{.data.token}}" | base64 --decode'.format(kubectl_cmd, secretname))
+def dump_token(secretname: str, kubeconfig: str, cluster_name: str) -> None:
+    os.system('{} get secret --kubeconfig={} --cluster={} {} -o jsonpath="{{.data.token}}" | base64 --decode'.format(kubectl_cmd,  kubeconfig, cluster_name, secretname))
 
 if __name__ == "__main__":
 
     if len(sys.argv) == 1:
-        print("Usage: setup service-account | get-ca-cert | get-token --help", file=sys.stderr)
+        print("Usage: setup-spark-k8s service-account | get-ca-cert | get-token --help", file=sys.stderr)
         sys.exit(-1)
 
 
     if sys.argv[1] == 'service-account':
         if len(sys.argv) == 2 or sys.argv[2] == '-h' or sys.argv[2] == '--help':
-            print("Usage: setup-spark-k8s service-account account-name [namespace]", file=sys.stderr)
+            print("Usage: setup-spark-k8s service-account --kubeconfig kubeconfig-file-name --cluster cluster-name-in-kubeconfig account-name [namespace]", file=sys.stderr)
             sys.exit(-1)
-        
-        setup_user(username=sys.argv[2], namespace=sys.argv[3] if len(sys.argv) >= 4 else 'default')
+
+        if len(sys.argv) >= 7:
+            if sys.argv[2] == '--kubeconfig' and sys.argv[4] == '--cluster':
+                kubeconfig = sys.argv[3]
+                cluster_name = sys.argv[5]
+            elif sys.argv[2] == '--cluster' and sys.argv[4] == '--kubeconfig':
+                cluster_name = sys.argv[3]
+                kubeconfig = sys.argv[5]
+            else:
+                print("ERROR: Invalid Arguments.", file=sys.stderr)
+                sys.exit(-2)
+
+            username = sys.argv[6]
+            namespace = sys.argv[7] if len(sys.argv) >= 8 else 'default'
+        else:
+            print("ERROR: Invalid Arguments.", file=sys.stderr)
+            sys.exit(-2)
+
+        setup_user(username, namespace, kubeconfig, cluster_name)
 
     elif sys.argv[1] == 'get-ca-cert':
         if len(sys.argv) == 3 and (sys.argv[2] == '-h' or sys.argv[2] == '--help'):
-            print("Usage: setup-spark-k8s get-ca-cert --kubeconfig kubeconfig-file-name --cluster cluster-name > ca.crt", file=sys.stderr)
-            print("Usage: setup-spark-k8s get-ca-cert [kubeconfig]", file=sys.stderr)
+            print("Usage: setup-spark-k8s get-ca-cert --kubeconfig kubeconfig-file-name --cluster cluster-name-in-kubeconfig > ca.crt", file=sys.stderr)
             sys.exit(-1)
 
         if len(sys.argv) == 6:
@@ -90,7 +106,7 @@ if __name__ == "__main__":
         try:
             extract_ca_crt_from_kube_config(kubeconfig, cluster_name)
         except IOError as e:
-            print('\nERROR: Bad kubeconfig file provided. Or default kubeconfig file /home/{}/.kube/config not found.'.format(os.getlogin()))
+            print('\nERROR: Bad kubeconfig file. Or default kubeconfig file /home/{}/.kube/config not found.'.format(os.getlogin()))
             print('\n\n')
             print('Looks like kubectl is not setup. Please follow the instructions below to setup kubectl first!')
             print('	1. sudo snap install kubectl --classic')
@@ -101,10 +117,25 @@ if __name__ == "__main__":
 
     elif sys.argv[1] == 'get-token':
         if len(sys.argv) == 2 or sys.argv[2] == '-h' or sys.argv[2] == '--help':
-            print("Usage: setup-spark-k8s get-token secretname\nsecretname is one of the output names of [kubectl get secrets]", file=sys.stderr)
+            print("Usage: setup-spark-k8s get-token --kubeconfig kubeconfig-file-name --cluster cluster-name-in-kubeconfig secretname > token\nsecretname is one of the output names of [kubectl get secrets]", file=sys.stderr)
             sys.exit(-1)
 
-        dump_token(secretname=sys.argv[2])
+        if len(sys.argv) == 7:
+            if sys.argv[2] == '--kubeconfig' and sys.argv[4] == '--cluster':
+                kubeconfig = sys.argv[3]
+                cluster_name = sys.argv[5]
+            elif sys.argv[2] == '--cluster' and sys.argv[4] == '--kubeconfig':
+                cluster_name = sys.argv[3]
+                kubeconfig = sys.argv[5]
+            else:
+                print("ERROR: Invalid Arguments.", file=sys.stderr)
+                sys.exit(-2)
+
+            secretname = sys.argv[6]
+        else:
+            print("ERROR: Invalid Arguments.", file=sys.stderr)
+            sys.exit(-2)
+        dump_token(secretname, kubeconfig, cluster_name)
 
     else:
         print("ERROR: Invalid Arguments.", file=sys.stderr)
