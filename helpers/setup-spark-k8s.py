3417aa90a3ca66313e12c110ec922f8c58c0eb09
Abhishek Verma <abhishek.verma@canonical.com>
2022-10-17 13:44:43 +0530

    Redesign UX based on feedback. Kubernetes context based setup.

--- helpers/setup-spark-k8s.py
+++ helpers/setup-spark-k8s.py
@@ -5,113 +5,115 @@ import os
 import yaml
 import argparse
 import pwd
+from typing import Dict
 
-kubectl_cmd='{}/kubectl'.format(os.environ['SNAP'])
+KUBECTL_CMD= '{}/kubectl'.format(os.environ['SNAP'])
+USER_HOME_DIR_ENT_IDX = 5
 
-def print_help_for_missing_or_inaccessible_kubeconfig_file():
-    print('\nERROR: Bad kubeconfig file. Or default kubeconfig file {}/.kube/config not found.'.format(pwd.getpwuid(os.getuid())[5]))
+def print_help_for_missing_or_inaccessible_kubeconfig_file(kubeconfig: str):
+    print('\nERROR: Missing kubeconfig file {}. Or default kubeconfig file {}/.kube/config not found.'.format(kubeconfig, pwd.getpwuid(os.getuid())[5]))
     print('\n')
-    print('Looks like either kubernetes is not setup properly or default kubeconfig file is not accessible!')
+    print('Looks like either kubernetes is not set up properly or default kubeconfig file is not accessible!')
     print('Please take the following remedial actions.')
-    print('	1. Please setup kubernetes and make sure kubeconfig file is available, accessible and correct.')
+    print('	1. Please set up kubernetes and make sure kubeconfig file is available, accessible and correct.')
     print('	2. sudo snap connect spark-client:enable-kubeconfig-access')
 
-def extract_default_context_from_kubeconfig(kubeconfig: str) -> str:
-    with open(kubeconfig) as f:
-        kube_cfg = yaml.safe_load(f)
-        return kube_cfg['current-context']
+def print_help_for_bad_kubeconfig_file(kubeconfig: str):
+    print('\nERROR: Invalid or incomplete kubeconfig file {}. One or more of the following entries might be missing or invalid.\n'.format(kubeconfig))
+    print('	- current-context')
+    print('	- context.name')
+    print('	- context.namespace')
+    print('	- context.cluster')
+    print('	- cluster.name')
+    print('	- cluster.server')
+    print(' - cluster.certificate-authority-data')
+    print('Please take the following remedial actions.')
+    print('	1. Please set up kubernetes and make sure kubeconfig file is available, accessible and correct.')
+    print('	2. sudo snap connect spark-client:enable-kubeconfig-access')
 
-def try_extract_default_context_from_kubeconfig(kubeconfig: str) -> str:
-    try:
-        context_name = extract_default_context_from_kubeconfig(kubeconfig)
-    except IOError as e:
-        print_help_for_missing_or_inaccessible_kubeconfig_file()
-        sys.exit(-300)
-    return context_name
+def select_context_id(kube_cfg: Dict) -> int:
+    NO_CONTEXT = -1
+    SINGLE_CONTEXT = 0
+    context_names = [n['name'] for n in kube_cfg['contexts']]
 
+    selected_context_id = NO_CONTEXT
+    if len(context_names) == 1:
+        return SINGLE_CONTEXT
 
-def extract_default_namespace_from_kube_config(kubeconfig: str) -> None:
-    default_namespace = 'default'
-    with open(kubeconfig) as f:
-        kube_cfg = yaml.safe_load(f)
-        context_names = [n['name'] for n in kube_cfg['contexts']]
+    context_list_ux = '\n'.join(["{}. {}".format(context_names.index(a), a) for a in context_names])
+    while selected_context_id == NO_CONTEXT:
+        print (context_list_ux)
+        print('\nPlease select a kubernetes context by index:')
+        selected_context_id = input()
+
         try:
-            namespaces = [n['context']['namespace'] for n in kube_cfg['contexts']]
-        except KeyError:
-            return default_namespace
+           int(selected_context_id)
+        except ValueError:
+            print('Invalid context index selection, please try again....')
+            selected_context_id = NO_CONTEXT
+            continue
 
-        if len(context_names) > 1:
-            try:
-                context_id = context_names.index(kube_cfg['current-context'])
-            except ValueError:
-                return default_namespace
-        elif len(context_names) == 1:
-            context_id = 0
-        else:
-            return default_namespace
+        if int(selected_context_id) not in range(len(context_names)):
+            print('Invalid context index selection, please try again....')
+            selected_context_id = NO_CONTEXT
+            continue
 
-        return namespaces[int(context_id)]
+    return int(selected_context_id)
 
-def try_extract_default_namespace_from_kube_config(kubeconfig: str) -> str:
+def get_defaults_from_kubeconfig(kubeconfig: str, context: str = None) -> Dict:
     try:
-        namespace = extract_default_namespace_from_kube_config(kubeconfig)
-    except IOError as e:
-        return 'default'
+        with open(kubeconfig) as f:
+            kube_cfg = yaml.safe_load(f)
+            context_names = [n['name'] for n in kube_cfg['contexts']]
+            # namespaces = [n['context']['namespace'] for n in kube_cfg['contexts']]
+            certs = [n['cluster']['certificate-authority-data'] for n in kube_cfg['clusters']]
+            current_context = context or kube_cfg['current-context']
+    except IOError as ioe:
+        print_help_for_missing_or_inaccessible_kubeconfig_file(kubeconfig)
+        sys.exit(-1)
+    except KeyError as ke:
+        print_help_for_bad_kubeconfig_file(kubeconfig)
+        sys.exit(-2)
 
-    return namespace
+    try:
+        context_id = context_names.index(current_context)
+    except ValueError:
+        print(f'WARNING: Current context in provided kubeconfig file {kubeconfig} is invalid!')
+        print(f'\nProceeding with explicit context selection....')
+        context_id = select_context_id(kube_cfg)
 
-def extract_ca_crt_from_kube_config(kubeconfig: str, context_name: str = None) -> None:
-    with open(kubeconfig) as f:
-        kube_cfg = yaml.safe_load(f)
-        context_names = [n['name'] for n in kube_cfg['contexts']]
-        cluster_names = [n['name'] for n in kube_cfg['clusters']]
-        certs = [n['cluster']['certificate-authority-data'] for n in kube_cfg['clusters']]
+    defaults = {}
+    defaults['context'] = context_names[context_id]
+    defaults['namespace'] = 'default' # namespaces[context_id]
+    defaults['cert'] = certs[context_id]
+    defaults['config'] = kubeconfig
+    defaults['user'] = 'spark'
 
-        if context_name:
-            try:
-                context_id = context_names.index(context_name)
-            except ValueError:
-                print('Invalid context selection, please try again....')
-                sys.exit(-100)
-        elif len(context_names) > 1:
-            print ('\n'.join(["{}. {}".format(context_names.index(a), a) for a in context_names]))
-            print('\nPlease select a context id:')
-            context_id = input()
+    return defaults
 
-            try:
-               int(context_id)
-            except ValueError:
-                print('Invalid context selection, please try again....')
-                sys.exit(-100)
+def set_up_user(username: str, name_space: str, defaults: Dict) -> None:
+    namespace = name_space or defaults['namespace']
+    kubeconfig = defaults['config']
+    context_name = defaults['context']
 
-            if int(context_id) not in range(len(context_names)):
-                print('Invalid cluster selection, please try again....')
-                sys.exit(-100)
-        elif len(context_names) == 1:
-            context_id = 0
-        else:
-            print('ERROR: No contexts found in kubeconfig file. Please provide a valid kubeconfig file!')
-            sys.exit(-100)
-
-        os.system(f'\n\necho {certs[int(context_id)]} | base64 --decode')
-
-
-def setup_user(username: str, namespace: str, kubeconfig: str, context_name: str) -> None:
-    #clusterrolebindingname = username + '-role'
-    #clusterroleaccess='view'
     rolebindingname = username + '-role'
     roleaccess='view'
-    os.system(f"{kubectl_cmd} create serviceaccount --kubeconfig={kubeconfig} --context={context_name} {username} --namespace={namespace}")
-    #os.system(f"{kubectl_cmd} create clusterrolebinding --kubeconfig={kubeconfig} --context={context_name} {clusterrolebindingname} --clusterrole={clusterroleaccess}  --serviceaccount={namespace}:{username} --namespace={namespace}")
-    os.system(f"{kubectl_cmd} create rolebinding --kubeconfig={kubeconfig} --context={context_name} {rolebindingname} --clusterrole={roleaccess}  --serviceaccount={namespace}:{username} --namespace={namespace}")
+    os.system(f"{KUBECTL_CMD} create serviceaccount --kubeconfig={kubeconfig} --context={context_name} {username} --namespace={namespace}")
+    os.system(f"{KUBECTL_CMD} create rolebinding --kubeconfig={kubeconfig} --context={context_name} {rolebindingname} --role={roleaccess}  --serviceaccount={namespace}:{username} --namespace={namespace}")
 
-def generate_token(serviceaccountname: str, namespace: str, kubeconfig: str, context_name: str) -> None:
-    os.system(f'{kubectl_cmd} create token {serviceaccountname} --namespace {namespace} --kubeconfig={kubeconfig} --context={context_name}')
+def print_cert(defaults: Dict) -> None:
+    cert_encoded = defaults['cert']
+    os.system(f'\n\necho {cert_encoded} | base64 --decode')
+
+def generate_token(serviceaccountname: str, name_space: str, defaults: Dict) -> None:
+    namespace = name_space or defaults['namespace']
+    kubeconfig = defaults['config']
+    context_name = defaults['context']
+    os.system(f'{KUBECTL_CMD} create token {serviceaccountname} --namespace {namespace} --kubeconfig={kubeconfig} --context={context_name}')
 
 if __name__ == "__main__":
-
-    default_kubeconfig = '{}/.kube/config'.format(pwd.getpwuid(os.getuid())[5])
-    default_namespace = try_extract_default_namespace_from_kube_config(default_kubeconfig)
+    USER_HOME_DIR = pwd.getpwuid(os.getuid())[USER_HOME_DIR_ENT_IDX]
+    DEFAULT_KUBECONFIG = f'{USER_HOME_DIR}/.kube/config'
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--kubeconfig", default=None, help='Kubernetes configuration file')
@@ -122,42 +124,28 @@ if __name__ == "__main__":
     #  subparser for service-account
     parser_account = subparsers.add_parser('service-account')
     parser_account.add_argument('--username', default='spark', help='Service account username to be created in kubernetes. Default is spark')
-    parser_account.add_argument('--namespace', default=default_namespace, help='Namespace for the service account to be created in kubernetes. Default is default namespace')
+    parser_account.add_argument('--namespace', default=None, help='Namespace for the service account to be created in kubernetes. Default is default namespace')
 
     #  subparser for CA certificate
     parser_certificate = subparsers.add_parser('get-ca-cert')
 
     #  subparser for OAuth token
     parser_token = subparsers.add_parser('get-token')
-    parser_token.add_argument('--serviceaccount', default='spark', help='Service account name for which the Oauth token is to be fetched. Default is spark account.')
-    parser_token.add_argument('--namespace', default=default_namespace, help='Namespace for the service account for which the Oauth token is to be fetched. Default is default namespace')
+    parser_token.add_argument('--username', default='spark', help='Service account name for which the Oauth token is to be fetched. Default is spark account.')
+    parser_token.add_argument('--namespace', default=None, help='Namespace for the service account for which the Oauth token is to be fetched. Default is default namespace')
 
     args = parser.parse_args()
 
-    if args.kubeconfig is not None:
-        kubeconfig = args.kubeconfig
-    else:
-        kubeconfig = default_kubeconfig
-
-    if args.context is not None:
-        context_name = args.context
-    else:
-        context_name = try_extract_default_context_from_kubeconfig(kubeconfig)
+    defaults = get_defaults_from_kubeconfig(args.kubeconfig or DEFAULT_KUBECONFIG, args.context)
+    print(defaults)
 
     if args.action == 'service-account':
-        username=args.username
-        namespace = args.namespace
-        # print ('kubeconfig={} context={} username={}, namespace={}'.format(kubeconfig, context_name, username, namespace))
-        setup_user(username, namespace, kubeconfig, context_name)
+        username = args.username
+        namespace = args.namespace or defaults['namespace']
+        set_up_user(username, namespace, defaults)
     elif args.action == 'get-ca-cert':
-        # print ('kubeconfig={} context={}'.format(kubeconfig, context_name))
-        try:
-            extract_ca_crt_from_kube_config(kubeconfig, context_name)
-        except IOError as e:
-            print_help_for_missing_or_inaccessible_kubeconfig_file()
-            sys.exit(-400)
+        print_cert(defaults)
     elif args.action == 'get-token':
-        serviceaccountname=args.serviceaccount
-        namespace = args.namespace
-        # print ('kubeconfig={} context={} serviceaccount={}, namespace={}'.format(kubeconfig, context_name, serviceaccountname, namespace))
-        generate_token(serviceaccountname, namespace, kubeconfig, context_name)
+        username = args.username
+        namespace = args.namespace or defaults['namespace']
+        generate_token(username, namespace, defaults)
--- helpers/spark-submit.py
+++ helpers/spark-submit.py
@@ -4,25 +4,68 @@ import sys
 import os
 import pwd
 import subprocess
+from typing import List,Dict
+
+USER_HOME_DIR_ENT_IDX = 5
+
+def expand_arg(arg: str) -> str:
+    if os.environ.get(arg):
+        return os.environ[arg]
+    else:
+        return arg
+def parse_config_args(args: List[str]) -> Dict:
+    i = 0
+    conf = dict()
+    remaining_args = []
+
+    while (i < len(args)):
+        if (args[i] == '--master'):
+            i += 1
+            conf['spark.master'] = expand_arg(args[i])
+        elif (args[i] == '--conf'):
+            i += 1
+            try:
+                kv = args[i].split('=')
+                conf[kv[0]] = expand_arg(kv[1])
+            except IndexError as e:
+                print('ERROR: Configuration related arguments parsing error. Please check input aarguments and try again.')
+                sys.exit(-2)
+        else:
+            remaining_args.append(args[i])
+
+        i += 1
+
+    return conf
 
 if __name__ == "__main__":
-    os.environ["HOME"] = pwd.getpwuid(os.getuid())[5]
+    os.environ["HOME"] = pwd.getpwuid(os.getuid())[USER_HOME_DIR_ENT_IDX]
 
     if os.environ.get('SPARK_HOME') is None:
         os.environ['SPARK_HOME'] = os.environ['SNAP']
 
     submit_args = sys.argv[1:]
-    # if '--master' not in sys.argv:
-    #     default_kubeconfig = '{}/.kube/config'.format(os.environ["HOME"])
-    #     default_kubectl_cmd = '{}/kubectl'.format(os.environ['SNAP'])
-    #     default_master_kubectl_cmd = f"{default_kubectl_cmd} --kubeconfig {default_kubeconfig} config view -o jsonpath=\"{{.clusters[0]['cluster.server']}}\""
-    #     try:
-    #        default_master = subprocess.check_output(default_master_kubectl_cmd, shell=True)
-    #        master_args = ['--master', 'k8s://' + default_master.decode("utf-8")]
-    #        submit_args = master_args + submit_args
-    #     except subprocess.CalledProcessError as e:
-    #        print('ERROR: Cannot determine default kubernetes control plane url. Probably bad or missing kubeconfig file {}'.format(default_kubeconfig))
-    #        sys.exit(-1)
+
+    conf = parse_config_args(submit_args)
+
+    if 'spark.master' not in conf.keys():
+        DEFAULT_KUBECONFIG = '{}/.kube/config'.format(os.environ["HOME"])
+        kubeconfig = os.environ.get('KUBECONFIG') or DEFAULT_KUBECONFIG
+        kubectl_cmd = '{}/kubectl'.format(os.environ['SNAP'])
+
+        context = conf.get('spark.kubernetes.context')
+        if context:
+            master_kubectl_cmd = f"{kubectl_cmd} --kubeconfig {kubeconfig} --context {context} config view --minify -o jsonpath=\"{{.clusters[0]['cluster.server']}}\""
+        else:
+            master_kubectl_cmd = f"{kubectl_cmd} --kubeconfig {kubeconfig} config view --minify -o jsonpath=\"{{.clusters[0]['cluster.server']}}\""
+
+        try:
+           default_master = subprocess.check_output(master_kubectl_cmd, shell=True)
+           master_args = ['--master', 'k8s://' + default_master.decode("utf-8")]
+           submit_args = master_args + submit_args
+        except subprocess.CalledProcessError as e:
+           print(f'ERROR: Cannot determine default kubernetes control plane url. Probably bad or missing kubeconfig file {kubeconfig}')
+           print(f'Please set the KUBECONFIG environment variable to point to the kubeconfig. Or fix the default kubeconfig {kubeconfig}')
+           sys.exit(-1)
 
     submit_cmd = '{}/bin/spark-submit'.format(os.environ['SPARK_HOME'])
 
