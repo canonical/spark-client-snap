78c31f1f4e926c06b6312088778c880a8686b19a
Abhishek Verma <abhishek.verma@canonical.com>
2022-10-21 16:04:06 +0530

    DPE-505: Added help to spark-submit options. Expand arg functionality simplified.

--- helpers/spark-submit.py
+++ helpers/spark-submit.py
@@ -11,27 +11,21 @@ USER_HOME_DIR_ENT_IDX = 5
 EXIT_CODE_BAD_KUBECONFIG = -100
 EXIT_CODE_BAD_CONF_ARG = -200
 
-def expand_arg(arg: str) -> str:
-    if os.environ.get(arg):
-        return os.environ[arg]
-    else:
-        return arg
-
 if __name__ == "__main__":
     logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s', level=logging.INFO)
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--master", default=None, type=str)
-    parser.add_argument("--properties-file", default=None, type=str)
-    parser.add_argument("--name", default="spark-app", type=str)
-    parser.add_argument("--deploy-mode", default="client", type=str)
-    parser.add_argument("--class", default=None, type=str)
-    parser.add_argument("--conf", action="append", type=str)
+    parser.add_argument("--master", default=None, type=str, help='Kubernetes control plane uri.')
+    parser.add_argument("--properties-file", default=None, type=str, help='Spark default configuration properties file.')
+    parser.add_argument("--name", default="spark-app", type=str, help='Spark application\'s name.')
+    parser.add_argument("--deploy-mode", default="client", type=str, help='Deployment mode for job submission. Default is \'client\'.')
+    parser.add_argument("--class", default=None, type=str, help='Java class of the application to be launched.')
+    parser.add_argument("--conf", action="append", type=str, help='Configuration properties to override the corresponding defaults.')
     parser.add_argument("extra_args", nargs="*")
     args = parser.parse_args()
 
     os.environ["HOME"] = pwd.getpwuid(os.getuid())[USER_HOME_DIR_ENT_IDX]
-    if os.environ.get('SPARK_HOME') is None:
+    if os.environ.get('SPARK_HOME') is None or os.environ.get('SPARK_HOME') == '':
         os.environ['SPARK_HOME'] = os.environ['SNAP']
 
     submit_args = sys.argv[1:]
@@ -41,9 +35,9 @@ if __name__ == "__main__":
         for c in args.conf:
             try:
                 kv = c.split('=')
-                conf[kv[0]] = expand_arg(kv[1])
+                conf[kv[0]] = os.environ.get(kv[1], kv[1])
             except IndexError as e:
-                logging.error('ERROR: Configuration related arguments parsing error. Please check input aarguments and try again.')
+                logging.error('Configuration related arguments parsing error. Please check input arguments and try again.')
                 sys.exit(EXIT_CODE_BAD_CONF_ARG)
 
     if not args.master:
@@ -63,7 +57,7 @@ if __name__ == "__main__":
            master_args = ['--master', 'k8s://' + default_master.decode("utf-8")]
            submit_args = master_args + submit_args
         except subprocess.CalledProcessError as e:
-           logging.error(f'ERROR: Cannot determine default kubernetes control plane url. Probably bad or missing kubeconfig file {kubeconfig}')
+           logging.error(f'Cannot determine default kubernetes control plane url. Probably bad or missing kubeconfig file {kubeconfig}')
            logging.warning(f'Please set the KUBECONFIG environment variable to point to the kubeconfig. Or fix the default kubeconfig {kubeconfig}')
            logging.error(e.output)
            sys.exit(EXIT_CODE_BAD_KUBECONFIG)
