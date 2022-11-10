65e5fa4ebbf84d24f28631f4207c08b1e7e5714d
Abhishek Verma <abhishek.verma@canonical.com>
2022-11-10 15:08:29 +0530

    DPE-839: Making UX consistent with spark-submit, autodetecting k8s master and accepting properties file as override.

--- helpers/spark-shell.py
+++ helpers/spark-shell.py
@@ -12,16 +12,39 @@ if __name__ == "__main__":
     logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s', level=logging.INFO)
 
     parser = argparse.ArgumentParser()
+    parser.add_argument("--master", default=None, type=str, help='Kubernetes control plane uri.')
     parser.add_argument("--deploy-mode", default="client", type=str, help='Deployment mode for spark shell. Default is \'client\'.')
+    parser.add_argument("--properties-file", default=None, type=str, help='Spark default configuration properties file.')
     args, extra_args = parser.parse_known_args()
 
     os.environ["HOME"] = pwd.getpwuid(os.getuid())[USER_HOME_DIR_ENT_IDX]
     if os.environ.get('SPARK_HOME') is None or os.environ.get('SPARK_HOME') == '':
         os.environ['SPARK_HOME'] = os.environ['SNAP']
 
-    SPARK_HOME = os.environ['SPARK_HOME']
-    SPARK_SHELL_ARGS = ' '.join(extra_args)
-    SCALA_HISTORY_FILE = utils.get_scala_shell_history_file()
-    shell_cmd = f'{SPARK_HOME}/bin/spark-shell --deploy-mode client --conf spark.driver.extraJavaOptions=\'-Dscala.shell.histfile={SCALA_HISTORY_FILE}\' {SPARK_SHELL_ARGS}'
-    logging.debug(shell_cmd)
-    os.system(shell_cmd)
+    STATIC_DEFAULTS_CONF_FILE = utils.get_static_defaults_conf_file()
+    DYNAMIC_DEFAULTS_CONF_FILE = utils.get_dynamic_defaults_conf_file()
+    ENV_DEFAULTS_CONF_FILE = utils.get_env_defaults_conf_file()
+
+    snap_static_defaults = utils.read_property_file(STATIC_DEFAULTS_CONF_FILE)
+    setup_dynamic_defaults = utils.read_property_file(DYNAMIC_DEFAULTS_CONF_FILE) if os.path.isfile(DYNAMIC_DEFAULTS_CONF_FILE) else dict()
+    env_defaults = utils.read_property_file(ENV_DEFAULTS_CONF_FILE) if ENV_DEFAULTS_CONF_FILE and os.path.isfile(ENV_DEFAULTS_CONF_FILE) else dict()
+    props_file_arg_defaults = utils.read_property_file(args.properties_file) if args.properties_file else dict()
+
+    with utils.UmaskNamedTemporaryFile(mode = 'w', prefix='spark-conf-', suffix='.conf') as t:
+        defaults = utils.merge_configurations([snap_static_defaults, setup_dynamic_defaults, env_defaults, props_file_arg_defaults])
+        SCALA_HISTORY_FILE = utils.get_scala_shell_history_file()
+        defaults['spark.driver.extraJavaOptions'] = f'-Dscala.shell.histfile={SCALA_HISTORY_FILE}'
+
+        logging.debug(f'Spark props available for reference at {utils.get_snap_temp_dir()}{t.name}\n')
+        utils.write_property_file(t.file, defaults, log=True)
+        t.flush()
+
+        shell_args = [ f'--master {args.master or utils.autodetect_kubernetes_master(defaults)}',
+                       f'--deploy-mode client',
+                       f'--properties-file {t.name}'] + extra_args
+
+        SPARK_HOME = os.environ['SPARK_HOME']
+        SPARK_SHELL_ARGS = ' '.join(shell_args)
+        shell_cmd = f'{SPARK_HOME}/bin/spark-shell {SPARK_SHELL_ARGS}'
+        logging.info(shell_cmd)
+        os.system(shell_cmd)
