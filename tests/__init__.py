ab9a5c827d879d310547f8169cd51ae6422688cc
Enrico Deusebio <edeusebio85@gmail.com>
2022-11-29 14:52:20 +0100

    [MISC] Adding Python Integration Tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,6 +1,6 @@
 import os
 import random
-from unittest import TestCase
+from unittest import TestCase, skipIf
 
 from helpers.utils import create_dir_if_not_exists  # type: ignore
 
@@ -8,6 +8,8 @@ test_path = os.path.dirname(os.path.abspath(__file__))
 
 DATA_FOLDER = os.path.join(test_path, "resources", "data")
 
+integration_test_flag = bool(int(os.environ.get("IE_TEST", "0")))
+integration_test = skipIf(integration_test_flag is False, "Integration test, to be skipped when running unittests")
 
 class UnittestWithTmpFolder(TestCase):
     TMP_FOLDER = os.path.join("/tmp", "%032x" % random.getrandbits(128))
