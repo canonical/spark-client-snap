5c69f7655d3f56a4985887d81cc22dab133e8400
Enrico Deusebio <enrico.deusebio@canonical.com>
2023-10-12 14:50:47 +0200

    fix home path

--- tests/integration/ie-tests.sh
+++ tests/integration/ie-tests.sh
@@ -210,9 +210,9 @@ setup_test_pod() {
 
   MY_KUBE_CONFIG=$(cat /home/${USER}/.kube/config)
 
-  kubectl exec testpod -- /bin/bash -c 'mkdir /home/spark/.kube'
-  kubectl exec testpod -- env KCONFIG="$MY_KUBE_CONFIG" /bin/bash -c 'echo "$KCONFIG" > /home/spark/.kube/config'
-  kubectl exec testpod -- /bin/bash -c 'cat /home/spark/.kube/config'
+  kubectl exec testpod -- /bin/bash -c 'mkdir -p ~/.kube'
+  kubectl exec testpod -- env KCONFIG="$MY_KUBE_CONFIG" /bin/bash -c 'echo "$KCONFIG" > ~/.kube/config'
+  kubectl exec testpod -- /bin/bash -c 'cat ~/.kube/config'
 }
 
 teardown_test_pod() {
