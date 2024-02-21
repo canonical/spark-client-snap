4d77df3b11c5a9a33047ce84ca346d3cf42769b5
Bikalpa Dhakal <bikalpa.dhakal@canonical.com>
2024-02-21 16:04:01 +0545

    Add retry logic

--- tests/integration/config-aws-cli.sh
+++ tests/integration/config-aws-cli.sh
@@ -9,14 +9,23 @@ SECRET_KEY=$(kubectl get secret -n minio-operator microk8s-user-1 -o jsonpath='{
 
 get_s3_endpoint(){
     # Get S3 endpoint from MinIO
-    kubectl get service minio -n minio-operator -o jsonpath='{.spec.clusterIP}' 
+    kubectl get service minioo -n minio-operator -o jsonpath='{.spec.clusterIP}' 
 }
 
 # Wait for `minio` service to be ready and S3 endpoint to be available
-until get_s3_endpoint &> /dev/null ; do
+retries=0
+max_retries=20
+until [ "$retries" -ge $max_retries ]
+do
+    get_s3_endpoint &> /dev/null && break
+    retries=$((retries+1)) 
     echo "Waiting for minio service to be up and running..."
     sleep 5
 done
+if [ "$retries" -ge $max_retries ]; then
+    echo "Maximum number of retries ($max_retries) reached. Service 'minio' is not available."
+    exit 1
+fi
 
 S3_ENDPOINT=$(get_s3_endpoint)
 
@@ -28,8 +37,19 @@ aws configure set aws_secret_access_key $SECRET_KEY
 aws configure set default.region $DEFAULT_REGION
 aws configure set endpoint_url "http://$S3_ENDPOINT"
 
-until aws s3 ls &> /dev/null; do
+
+retries=0
+max_retries=20
+until [ "$retries" -ge $max_retries ]
+do
+    aws s3 ls &> /dev/null && break
+    retries=$((retries+1)) 
     echo "Waiting for MinIO credentials to be set..."
     sleep 5
 done
+if [ "$retries" -ge $max_retries ]; then
+    echo "Maximum number of retries ($max_retries) reached. AWS CLI credentials could not be set."
+    exit 1
+fi
+
 echo "AWS CLI credentials set successfully"
