2b19960d0a26f82361d8caf2459067b4379c3427
welpaolo <paolo.sottovia@canonical.com>
2026-01-21 15:26:12 +0100

    [PRA-144] Add arm64 support (#138)

--- tests/integration/setup-azure-cli.sh
+++ tests/integration/setup-azure-cli.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
-echo "Installing Azure CLI..."
-sudo snap install azcli
+if ! command -v az >/dev/null 2>&1
+then
+    cat << EOF
+'az' command could not be found.
+Please install az using either:
+- pipx install azure-cli
+- uv tool install azure-cli --prerelease=allow
+- snap install azcli
+  snap alias azcli az
+EOF
+
+    exit 1
+fi
 
 
 # Early check to see if the two required environment variables are set.
@@ -14,7 +25,7 @@ echo "The variables AZURE_STORAGE_ACCOUNT and AZURE_STORAGE_KEY are found to be 
 
 # Test if the credentials are correct by listing containers
 echo "Testing Azure Storage credentials..."
-if ! azcli storage container list > /dev/null 2>&1; then
+if ! az storage container list > /dev/null 2>&1; then
   echo "Error: Invalid Azure Storage credentials."
   exit 1
 fi
