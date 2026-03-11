d151e6205b6a89799133b753a521abbc05885f2d
Alex Batisse <alex.batisse@canonical.com>
2026-03-11 16:26:02 +0100

    [PRA-211] Reorder PYTHONPATH to give priority to the snap's stdlib (#146)
    * refactor: Use spark8t entrypoints and reorder PYTHONPATH
    * refactor: Remove unneeded plugs for import-certificate app
    * chore: Add missing fields in snapcraft.yaml
    * chore: Bump spark8t to 1.4.0

--- snap/local/etc/spark8t/launcher.sh
+++ snap/local/etc/spark8t/launcher.sh
@@ -4,4 +4,8 @@ if [[ ! -n "$KUBECONFIG" ]]; then
   KUBECONFIG="$SNAP_REAL_HOME/.kube/config"
 fi
 
+if [[ -n "$PYTHONPATH" ]]; then
+  PYTHONPATH="/usr/lib/python310.zip:/usr/lib/python3.10:/usr/lib/python3.10/lib-dynload:$SNAP/lib/python3.10/site-packages:$PYTHONPATH"
+fi
+
 KUBECONFIG=$KUBECONFIG exec "$@"
