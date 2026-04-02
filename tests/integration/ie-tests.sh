01001b0f1bf4909a148a8bc8e64c77d60b4f7a83
Bikalpa Dhakal <bikalpa.dhakal@canonical.com>
2026-04-02 09:14:52 +0545

    Fix regular expression in catching result

--- tests/integration/ie-tests.sh
+++ tests/integration/ie-tests.sh
@@ -186,7 +186,7 @@ run_spark_shell() {
       --conf spark.executor.instances=2 \
       --namespace ${NAMESPACE})" \
       > spark-shell.out
-  pi=$(cat spark-shell.out  | grep "^Pi is roughly" | rev | cut -d' ' -f1 | rev | cut -c 1-3)
+  pi=$(cat spark-shell.out  | grep "Pi is roughly" | rev | cut -d' ' -f1 | rev | cut -c 1-3)
   echo -e "Spark-shell Pi Job Output: \n ${pi}"
   rm spark-shell.out
   validate_pi_value $pi
